c0d5721ea4ccb329015b0222490f0f25e8e5ffe1
Nick Dawson <37987430+NicholasDawson@users.noreply.github.com>
2023-03-07 21:03:33 +0000

    Update headshot, projects, and resume.

--- src/src/components/Hero.vue
+++ src/src/components/Hero.vue
@@ -1,33 +1,45 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
   <div class="px-4 py-3 my-3 text-center">
     <img
       class="d-block mx-auto mb-4 rounded-circle"
-      src="/images/headshot.jpg"
+      src="/images/headshot.webp"
       alt="Headshot"
-      width="100"
-      height="100"
+      width="175"
+      height="175"
     />
     <h1 class="display-5 fw-bold">Nick Dawson</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">
-        Software Engineering student at RIT with a passion for helping everyone with efficient and useful software.
-        Constantly learning new things and is interested in web development and cybersecurity.
-        In my free time I climb rocks, go hiking, play games, and work on personal software projects.
+        Software Engineering student at RIT with a passion for helping everyone
+        with efficient and useful software. Constantly learning new things and
+        is interested in web development and cybersecurity. In my free time I
+        climb rocks, go hiking, play games, and work on personal software
+        projects.
       </p>
       <div class="gap-2 d-flex justify-content-center">
-        <a href="https://github.com/nicholasdawson" target="_blank" title="GitHub">
+        <a
+          href="https://github.com/nicholasdawson"
+          target="_blank"
+          title="GitHub"
+        >
           <i class="bi-github social-icon"></i>
         </a>
 
-        <a href="https://www.linkedin.com/in/nicholasmdawson/" target="_blank" title="LinkedIn">
+        <a
+          href="https://www.linkedin.com/in/nicholasmdawson/"
+          target="_blank"
+          title="LinkedIn"
+        >
           <i class="bi-linkedin social-icon"></i>
         </a>
 
-        <a href="mailto:contact-website@ndawson.me" target="_blank" title="Email Me">
+        <a
+          href="mailto:contact-website@ndawson.me"
+          target="_blank"
+          title="Email Me"
+        >
           <i class="bi-envelope-fill social-icon"></i>
         </a>
 
--- src/src/views/ProjectsView.vue
+++ src/src/views/ProjectsView.vue
@@ -1,84 +1,112 @@
 <script setup>
-import Project from '@/components/Project.vue';
+import Project from "@/components/Project.vue";
 </script>
 
 <template>
-    <h1 class="display-3 text-center my-3">My Projects</h1>
+  <h1 class="display-3 text-center my-3">My Projects</h1>
 
-    <Project
-        title="froggy.app"
-        description="URL Shortener website that provides analytics to users. Created to learn new technologies and practice full stack web development with my best friend. The backend is private right now for security reasons, ask me if you are interested in the code!"
-        image="/screenshots/froggy-screenshot.png"
-        github="https://github.com/froggy-app"
-        link="https://froggy.app"
-        linkText="Try it!"
-        :badges="['Go', 'Gin', 'React', 'Redux', 'CockroachDB', 'Google App Engine']"
-    />
+  <Project
+    title="MathFlow"
+    description="A virtual workspace that allows users to interact with math equations in a unique and intuitive way using drag and drop/touch controls in our application. Created at the BrickHack9 Hackathon in 24 hours."
+    image="/screenshots/mathflow-demo.gif"
+    github="https://github.com/MathFlow-tech/mathflow-tech.github.io"
+    link="https://devpost.com/software/variflow"
+    linkText="Devpost"
+    :badges="[
+      'React',
+      'React-DnD',
+      'TypeScript',
+    ]"
+    reverse
+  />
 
-    <Project
-        title="Archiver for Google Photos"
-        description="An open-source command line tool to maintain an archive/mirror of your Google Photos library for backup purposes."
-        image="/screenshots/archiver-for-google-photos.png"
-        github="https://github.com/NicholasDawson/ArchiverForGooglePhotos"
-        :badges="['Python', 'Google APIs', 'Multithreading', 'SQLite']"
-        reverse
-    />
+  <Project
+    title="froggy.app"
+    description="URL Shortener website that provides analytics to users. Created to learn new technologies and practice full stack web development with my best friend. The backend is private right now for security reasons, ask me if you are interested in the code!"
+    image="/screenshots/froggy-screenshot.png"
+    github="https://github.com/froggy-app"
+    link="https://froggy.app"
+    linkText="Try it!"
+    :badges="[
+      'Go',
+      'Gin',
+      'React',
+      'Redux',
+      'CockroachDB',
+      'Google App Engine',
+    ]"
+  />
 
-    <Project
-        title="Chromebook Repair Management Tool"
-        description="An automation software suite that manages and tracks all steps of the Chromebook Repair Process at Portland CT Public Schools."
-        image="/screenshots/chromebook-repair-management-tool.png"
-        link="/documents/Capstone Presentation.pdf"
-        linkText="Presentation"
-        :badges="['Python', 'Google Sheets', 'PyQt5', 'Web Automation', 'PDF Filling']"
-    />
+  <Project
+    title="Archiver for Google Photos"
+    description="An open-source command line tool to maintain an archive/mirror of your Google Photos library for backup purposes."
+    image="/screenshots/archiver-for-google-photos.png"
+    github="https://github.com/NicholasDawson/ArchiverForGooglePhotos"
+    :badges="['Python', 'Google APIs', 'Multithreading', 'SQLite']"
+    reverse
+  />
 
-    <Project
-        title="Data Extractor for AniList"
-        description="A tool used to download most useful metadata and tags from an AniList.co entry."
-        image="/screenshots/data-extractor-for-anilist.png"
-        github="https://github.com/NicholasDawson/Data-Extractor-for-AniList"
-        :badges="['Python', 'Web Scraping', 'Selenium']"
-        reverse
-    />
+  <Project
+    title="Chromebook Repair Management Tool"
+    description="An automation software suite that manages and tracks all steps of the Chromebook Repair Process at Portland CT Public Schools."
+    image="/screenshots/chromebook-repair-management-tool.png"
+    link="/documents/Capstone Presentation.pdf"
+    linkText="Presentation"
+    :badges="[
+      'Python',
+      'Google Sheets',
+      'PyQt5',
+      'Web Automation',
+      'PDF Filling',
+    ]"
+  />
 
-    <Project
-        title="Dice Roller"
-        description="A simple dice rolling app with hand drawn graphics. This was my first Vue web app."
-        image="/screenshots/dice-roller.png"
-        github="https://github.com/NicholasDawson/VueExamples/tree/main/dice-roller"
-        link="https://ndawson.me/VueExamples/dice-roller/dist/"
-        :badges="['Vue', 'HTML']"
-    />
+  <Project
+    title="Data Extractor for AniList"
+    description="A tool used to download most useful metadata and tags from an AniList.co entry."
+    image="/screenshots/data-extractor-for-anilist.png"
+    github="https://github.com/NicholasDawson/Data-Extractor-for-AniList"
+    :badges="['Python', 'Web Scraping', 'Selenium']"
+    reverse
+  />
 
-    <Project
-        title="Weather App"
-        description="A responsive weather app with hand drawn weather graphics. I am learning Vue and Azure with this app."
-        image="/screenshots/weather-app.png"
-        github="https://github.com/NicholasDawson/VueExamples/tree/main/weather-app"
-        link="https://ndawson.me/VueExamples/weather-app/dist/"
-        :badges="['Vue', 'OpenWeather', 'Azure']"
-        reverse
-    />
+  <Project
+    title="Dice Roller"
+    description="A simple dice rolling app with hand drawn graphics. This was my first Vue web app."
+    image="/screenshots/dice-roller.png"
+    github="https://github.com/NicholasDawson/VueExamples/tree/main/dice-roller"
+    link="https://ndawson.me/VueExamples/dice-roller/dist/"
+    :badges="['Vue', 'HTML']"
+  />
 
-    <Project
-        title="WUPHF Weather"
-        description="A mobile app made for the BrickHack8 hackathon to tell you about today's weather in as many ways as possible!"
-        image="/screenshots/wuphf-weather.png"
-        github="https://github.com/DevanTurtle7/WUPHF-Weather"
-        link="https://devpost.com/software/wuphf-weather-nqk2u1"
-        linkText="Devpost"
-        :badges="['React Native', 'FastAPI', 'CockroachDB', 'Twilio', 'Python']"
-    />
+  <Project
+    title="Weather App"
+    description="A responsive weather app with hand drawn weather graphics. I am learning Vue and Azure with this app."
+    image="/screenshots/weather-app.png"
+    github="https://github.com/NicholasDawson/VueExamples/tree/main/weather-app"
+    link="https://ndawson.me/VueExamples/weather-app/dist/"
+    :badges="['Vue', 'OpenWeather', 'Azure']"
+    reverse
+  />
 
-    <Project
-        title="Twotor"
-        description="2nd Place Community Winner of the Meet & Hack 2022 hackathon. A tutoring web application where students help each other."
-        image="/screenshots/twotor.png"
-        github="https://github.com/DevanTurtle7/twotor"
-        link="https://devpost.com/software/twotor-ixyc3l"
-        linkText="Devpost"
-        :badges="['React', 'Flask', 'PostgreSQL', 'Python']"
-        reverse
-    />
+  <Project
+    title="WUPHF Weather"
+    description="A mobile app made for the BrickHack8 hackathon to tell you about today's weather in as many ways as possible!"
+    image="/screenshots/wuphf-weather.png"
+    github="https://github.com/DevanTurtle7/WUPHF-Weather"
+    link="https://devpost.com/software/wuphf-weather-nqk2u1"
+    linkText="Devpost"
+    :badges="['React Native', 'FastAPI', 'CockroachDB', 'Twilio', 'Python']"
+  />
+
+  <Project
+    title="Twotor"
+    description="2nd Place Community Winner of the Meet & Hack 2022 hackathon. A tutoring web application where students help each other."
+    image="/screenshots/twotor.png"
+    github="https://github.com/DevanTurtle7/twotor"
+    link="https://devpost.com/software/twotor-ixyc3l"
+    linkText="Devpost"
+    :badges="['React', 'Flask', 'PostgreSQL', 'Python']"
+    reverse
+  />
 </template>
